bce7225b0c70fe67ae3f8cd2a15c785ce90219b0
Shohruxshoh <shohhhhhhhhhhhhhhhhhhhh@gmail.com>
2024-05-30 15:12:38 +0500

    cors

--- quiz/settings.py
+++ quiz/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
